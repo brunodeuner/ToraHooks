fdcaf2f2cc7696529a424fdc8d650b0cec0063ee
brunodeuner <bruno_deuner@hotmail.com>
2022-10-15 19:41:59 -0300

    create pre-commit

---
+++ ToraHooks/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Abc;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        A();
+    }
+
+    private static void A() => Console.WriteLine("asd");
+
+    private static void B() => Console.WriteLine("asd");
+
+    private static void C() => Console.WriteLine("asd");
+}
